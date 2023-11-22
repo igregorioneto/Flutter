0fab23dd95bc8fa64581f651ef85d0a6d710d356
gregorio <igregoriodoneto@outlook.com>
2023-11-22 15:53:54 -0300

    Baixando o ebook na pasta temporária

--- reader_ebooks/lib/widgets/book_card.dart
+++ reader_ebooks/lib/widgets/book_card.dart
@@ -1,8 +1,12 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 import 'package:reader_ebooks/models/books.dart';
 import 'package:reader_ebooks/services/book_service.dart';
+import 'package:path/path.dart' as path;
 
 Widget bookCard(BuildContext context, Book book, BookService bookService) {
   return Consumer<BookService>(
@@ -11,37 +15,40 @@ Widget bookCard(BuildContext context, Book book, BookService bookService) {
         padding: EdgeInsets.only(bottom: 10.0),
         child: Stack(
           children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              padding: EdgeInsets.all(2.0),
-              margin: EdgeInsets.all(10.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Expanded(
-                    flex: 3,
-                    child: _bookImageCover(book.coverUrl),
-                  ),
-                  SizedBox(height: 8.0),
-                  Expanded(
-                    flex: 1,
-                    child: Text(
-                      book.title,
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                      overflow: TextOverflow.ellipsis,
+            GestureDetector(
+              onTap: () => _downloadEpub(context, book),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                padding: EdgeInsets.all(2.0),
+                margin: EdgeInsets.all(10.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      flex: 3,
+                      child: _bookImageCover(book.coverUrl),
                     ),
-                  ),
-                  SizedBox(height: 4.0),
-                  Expanded(
-                    flex: 1,
-                    child: Text(
-                      book.author,
-                      overflow: TextOverflow.ellipsis,
+                    SizedBox(height: 8.0),
+                    Expanded(
+                      flex: 1,
+                      child: Text(
+                        book.title,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                        overflow: TextOverflow.ellipsis,
+                      ),
                     ),
-                  ),
-                ],
+                    SizedBox(height: 4.0),
+                    Expanded(
+                      flex: 1,
+                      child: Text(
+                        book.author,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
             Positioned(
@@ -54,6 +61,29 @@ Widget bookCard(BuildContext context, Book book, BookService bookService) {
       );
     },
   );
+}
+
+void _downloadEpub(BuildContext context, Book book) async {
+  final status = await Permission.storage.request();
+
+  if(status.isGranted) {
+    final dio = Dio();
+    final tempDir = await getTemporaryDirectory();
+    final savePath = path.join(tempDir.path, '${book.title}.epub');
+
+    try {
+      final response = await dio.download(book.downloadUrl, savePath);
+      if (response.statusCode == 200) {
+        print('Download concluído: $savePath');
+      } else {
+        print('Erro durante o download');
+      }
+    } catch (e) {
+      print('Exceção durante o download: $e');
+    }
+  } else {
+    print('Permissão de armazenamento negada!');
+  }
 }
 
 Widget _bookImageCover(dynamic coverUrl) {
