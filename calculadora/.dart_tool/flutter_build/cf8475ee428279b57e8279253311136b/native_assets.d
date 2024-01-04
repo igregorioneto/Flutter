e78d4ae873949dad85568efa4088daee132a91c4
gregorio <igregoriodoneto@outlook.com>
2024-01-04 17:05:49 -0300

    feat: calculadora

---
+++ calculadora/.dart_tool/flutter_build/cf8475ee428279b57e8279253311136b/native_assets.d
@@ -0,0 +1,1 @@
+ C:\\Users\\igreg\\Documents\\Estudo\\Flutter\\calculadora\\.dart_tool\\flutter_build\\cf8475ee428279b57e8279253311136b\\native_assets.yaml: 
---
+++ helloworld/.dart_tool/flutter_build/eb46961bdae1c48a48baf47d9189a7d3/native_assets.d
@@ -0,0 +1,1 @@
+ C:\\Users\\igreg\\Documents\\Estudo\\Flutter\\helloworld\\.dart_tool\\flutter_build\\eb46961bdae1c48a48baf47d9189a7d3\\native_assets.yaml: 
